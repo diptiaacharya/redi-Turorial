94816afb6d3092dd26a3395e7ac8fe5a23e35b5f
Dipti Acharya <diptiacharya1984@gmail.com>
2022-11-14 17:21:52 +0100

    First repository. Added all files

---
+++ bmi_calculator.py
@@ -0,0 +1,4 @@
+weight = input("Please enter your weight in kg")
+height = input("Please enter your height in")
+bmi= weight/height * height
+print("Your BMI is="%s,)
---
+++ multiply.py
@@ -0,0 +1,19 @@
+
+multiply = 1
+count = 0
+while count < 5 :
+    n = input("please specify a number : ")
+    multiply = multiply * int(n)
+    count = count + 1
+print (" The Multiply is:  " + str(multiply))
+
+x = 0
+while (x < 100):
+    x+=2
+    print(x)
+
+for num in range(2, -5, -1):
+    print (num, end=", ")
+
+
+    
